5381ad2021f86dc47ae5d3ce0d953ef9078059ee
nsohit <52670389+nsohit@users.noreply.github.com>
2019-12-24 15:14:31 +0700

    Add files via upload

---
+++ neo6mkrgsm1400.ino
@@ -0,0 +1,56 @@
+//#include <SoftwareSerial.h>
+#include <TinyGPS.h> 
+#define SerialGPS Serial1
+
+TinyGPS gps;
+//SoftwareSerial ss(2, 3);
+
+void setup()
+{
+  Serial.begin(9600);
+  SerialGPS.begin(9600); //TEST GPS.
+
+}
+
+void loop()
+{
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (SerialGPS.available())
+    {
+      char c = SerialGPS.read();
+      // Serial.write(c); // hilangkan koment jika mau melihat data yang dikirim dari modul GPS
+      if (gps.encode(c)) // Parsing semua data
+        newData = true;
+    }
+  }
+
+  if (newData)
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    Serial.print("LAT=");
+    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    Serial.print(" LON=");
+    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+    Serial.print(" SAT=");
+    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+    Serial.print(" PREC=");
+    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+  }
+  
+  gps.stats(&chars, &sentences, &failed);
+  Serial.print(" CHARS=");
+  Serial.print(chars);
+  Serial.print(" SENTENCES=");
+  Serial.print(sentences);
+  Serial.print(" CSUM ERR=");
+  Serial.println(failed);
+  if (chars == 0)
+    Serial.println("** Tidak ada Data Masuk, Periksa Wiring **");
+}
